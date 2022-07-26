ed7ffd7664e0d32cdc97d0e3185956fb31946235
goofygamer <syadav2001@outlook.com>
2022-07-27 03:20:58 +0530

    Introduces the static paddles

--- solution.py
+++ solution.py
@@ -16,8 +16,10 @@ FPS = 60
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
+#Paddle variables for height and width
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100  
 
-
+#Object class Paddle
 class Paddle:
     COLOR = WHITE
 
@@ -30,18 +32,27 @@ class Paddle:
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
-
-def draw(win):
+#Draw the objects
+def draw(win, paddles):
     win.fill(BLACK)
+
+    for paddle in paddles: 
+        paddle.draw(win)
+    
     pygame.display.update()
 
+
+#Main class
 def main():
     run = True
     clock = pygame.time.Clock()
 
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
     while run:
         clock.tick(FPS)
-        draw(WIN) #Redraws 60 time a second
+        draw(WIN, [left_paddle, right_paddle]) #Redraws 60 time a second
 
         for event in pygame.event.get(): #All of the events that will occur
             if event.type == pygame.QUIT:
